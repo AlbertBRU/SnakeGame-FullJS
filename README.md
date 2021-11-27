c36c9a63485726ea0230edb6c1ab3c639c30dbb2
albert-bru <albertbru01@gmail.com>
2021-11-27 13:06:40 +0100

    board complète To do animation

---
+++ README.md
@@ -0,0 +1,4 @@
+# Snake Full JS
+
+En réalisant un des exercices de ma formation O'clock, portant sur les interactions JS avec le dom, je me suis rendu compte que l'exercice n'etait pas loin du célèbre jeu rétro "snake".
+Je vais donc tenter de le reproduire du mieux possible afin qu'il soit jouable et amusant.
--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
 // App document
+
+var app = {
+
+  init: function() {
+    console.log('init');
+
+    app.drawBoard();
+  },
+
+  drawBoard:function () {
+    // target the div with id board to insert our rows
+    const board = document.getElementById('board')
+    console.log(board)
+
+    for (let row = 1; row < 21; row++) {
+      cellRow = document.createElement('div');
+      cellRow.classList.add('cellRow');
+      cellRow.id = 'row'+ row ;
+      board.append(cellRow);
+      // Once a raw is created, fill it with 6 cells
+      for (let cell = 1; cell < 21; cell++) {
+        cellToInsert = document.createElement('div');
+        cellToInsert.classList.add('cell');
+        cellToInsert.dataset.row = row;
+        cellToInsert.dataset.cell = cell;
+        cellRow.append(cellToInsert)
+      }
+    }
+  }
+
+}  
+
+document.addEventListener('DOMContentLoaded', app.init);
